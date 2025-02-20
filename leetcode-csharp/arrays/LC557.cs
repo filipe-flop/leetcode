9bb9b174766226c1e85d8f1af9ea49cb1e862432
Filipe de Oliveira <filipedeoliveira97@gmail.com>
2025-02-20 20:47:52 -0300

    557 better solution

--- leetcode-csharp/arrays/LC557.cs
+++ leetcode-csharp/arrays/LC557.cs
@@ -41,5 +41,30 @@ namespace leetcode_csharp.arrays
             }
             return new string(ret);
         }
+
+        public string ReverseWordsBetterSolution(string s)
+        {
+            char[] chars = s.ToCharArray();
+            int start = 0;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == ' ' || i == chars.Length - 1)
+                {
+                    int end = (i == chars.Length - 1 && chars[i] != ' ') ? i + 1 : i;
+                    while (start < end)
+                    {
+                        char temp = chars[start];
+                        chars[start] = chars[end - 1];
+                        chars[end - 1] = temp;
+                        start++;
+                        end--;
+                    }
+                    start = i + 1;
+                }
+            }
+
+            return new string(chars);
+        }
     }
 }
