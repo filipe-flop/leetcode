60a6311e1ea8aaa0a98173089cb4224676c0d317
Filipe de Oliveira <filipedeoliveira97@gmail.com>
2025-02-25 21:37:25 -0300

    611 valid triangle number

--- leetcode-csharp/Program.cs
+++ leetcode-csharp/Program.cs
@@ -1,1 +1,4 @@
 ﻿using leetcode_csharp.arrays.TwoPointers;
+
+LC611 lC611 = new LC611();
+lC611.TriangleNumber([4, 2, 3, 4]);
--- leetcode-csharp/arrays/TwoPointers/LC611.cs
+++ leetcode-csharp/arrays/TwoPointers/LC611.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace leetcode_csharp.arrays.TwoPointers
 {
-    internal class LC611
+    public class LC611
     {
+        // accepted
+        // 26ms
+        // beats 98.46%
+        public int TriangleNumber(int[] nums)
+        {
+            int ret = 0, left, right;
+
+            if (nums.Length < 3)
+                return ret;
+
+            Array.Sort(nums); // O(n log n) time | O(log n) space
+
+            for (int i = nums.Length - 1; i > 0; i--)
+            {
+                left = 0;
+                right = i - 1;
+                while (right > left)
+                {
+                    if (nums[left] + nums[right] > nums[i])
+                    {
+                        ret += right - left;
+                        right--;
+                    }
+                    else
+                    {
+                        left++;
+                    }
+                }
+            }
+            return ret;
+        }
     }
 }
