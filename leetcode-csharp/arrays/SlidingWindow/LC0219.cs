195f4f6c61d576fed0a62c49fa2b0b3bc1b76060
Filipe de Oliveira <filipedeoliveira97@gmail.com>
2025-03-06 20:07:07 -0300

    sliding window start

---
+++ leetcode-csharp/arrays/SlidingWindow/LC0219.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_csharp.arrays.SlidingWindow
+{
+    public class LC0219
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            return false;
+        }
+    }
+}
