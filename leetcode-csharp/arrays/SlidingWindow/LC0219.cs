f476c7cb93f5ae803ce03beb8d5942ecd0f47b7d
Filipe de Oliveira <filipedeoliveira97@gmail.com>
2025-03-08 20:34:33 -0300

    att 219

--- leetcode-csharp/arrays/SlidingWindow/LC0219.cs
+++ leetcode-csharp/arrays/SlidingWindow/LC0219.cs
@@ -10,12 +10,22 @@ namespace leetcode_csharp.arrays.SlidingWindow
     {
         public bool ContainsNearbyDuplicate(int[] nums, int k)
         {
-            int r = 0;
-            int l = 0;
-
-            while (r < nums.Length)
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
             {
+                if (!dictionary.ContainsKey(nums[i]))
+                {
+                    dictionary.Add(nums[i], i);
+                }
+                else
+                {
+                    if (Math.Abs(dictionary[nums[i]] - i) <= k)
+                    {
+                        return true;
+                    }
 
+                    dictionary[nums[i]] = i;
+                }
             }
 
             return false;
