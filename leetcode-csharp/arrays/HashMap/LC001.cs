b170f4ce918a318c61ca35109f39d786df5830a5
Filipe de Oliveira <filipedeoliveira97@gmail.com>
2025-03-16 13:51:01 -0300

    LC001

---
+++ leetcode-csharp/arrays/HashMap/LC001.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_csharp.arrays.HashMap
+{
+    public class LC001
+    {
+        //accepted
+        //runtime 1ms
+        //beats 98.14%
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int remainder = target - nums[i];
+                if (d.ContainsKey(remainder)) {
+                    return new int[] { d[remainder], i };
+                }
+                d[nums[i]] = i;
+            }
+            return new int[] {};
+        }
+    }
+}
